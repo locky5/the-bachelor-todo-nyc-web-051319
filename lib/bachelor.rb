5a92d65928df5e3f3a8cc036696c9386341b8b7e
Kevin Dai <daikevin19@yahoo.com>
2019-05-05 01:49:27 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,35 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = []
+  data[season].each do |contestant| # contestant refers to all info about each contestant
+    if contestant["status"] == "Winner"
+      first_name << contestant["name"].split(" ")[0] # splits name of winner
+    end
+  end
+  first_name[0] # return only the first value
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  full_name = []
+  data.each do |season, contestants|
+    contestants.each do |stats, values|
+      if stats["occupation"] == occupation
+        full_name << stats["name"]
+      end
+    end
+  end
+  full_name[0]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+  data.each do |season, contestants|
+    contestants.each do |stats, values|
+      if stats["hometown"] == hometown
+        counter += 1 
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
