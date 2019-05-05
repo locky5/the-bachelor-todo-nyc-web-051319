b9f843bd5e73e4ba5f634f5aeb044678c3cfae3e
Kevin Dai <daikevin19@yahoo.com>
2019-05-05 01:57:56 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,53 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = []
+  data[season].each do |contestant| # contestant refers to all info about each contestant
+    if contestant["status"] == "Winner"
+      first_name << contestant["name"].split(" ")[0] # splits name of winner
+    end
+  end
+  first_name[0] # return only the first value
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  full_name = []
+  data.each do |season, contestants|
+    contestants.each do |stats, values|
+      if stats["occupation"] == occupation
+        full_name << stats["name"]
+      end
+    end
+  end
+  full_name[0]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+  data.each do |season, contestants|
+    contestants.each do |stats, values|
+      if stats["hometown"] == hometown
+        counter += 1 
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |stats, values|
+      if stats["hometown"] == hometown
+        return stats["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_total = 0 
+  contestant_count = 0
+  data[season].each do |contestant|
+    age_total += contestant["age"].to_i # converts string to integer
+    contestant_count += 1
+  end
+  return (age_total/contestant_count.to_f).round # to_f makes int into float, 'round', or round(0) rounds the final value to 0 decimal places
 end
